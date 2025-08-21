a84af930ad3fd38cb755c958cd61130635b5d113
ingJuanArenas <juanarenasing@gmail.com>
2025-08-21 16:17:34 -0500

    some changes related with the new components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TaskPage } from './features/components/tasks-page/tasks-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: "tasks", pathMatch:'full' },
+  { path: 'tasks', loadChildren: ()=> import('./features/tasks-route').then(c=> c.tasks_routes) }
+];
