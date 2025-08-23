38741621fb2a3813cc1a7d5b99fda0f4e1172190
ingJuanArenas <juanarenasing@gmail.com>
2025-08-23 09:27:08 -0500

    Modifing routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { Loginpage } from './features/components/loginpage/loginpage';
 import { TaskPage } from './features/components/tasks-page/tasks-page';
+import { AuthGuard } from './features/service/auth-guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: "tasks", pathMatch:'full' },
-  { path: 'tasks', loadChildren: ()=> import('./features/tasks-route').then(c=> c.tasks_routes) }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Loginpage },
+  { path: 'tasks', loadChildren: ()=> import('./features/tasks-route').then(c=> c.tasks_routes), canActivate: [AuthGuard] }
 ];
